78215d21ac00dc7a41007cb3f8d70c0cda36bf89
Rafał Całka <25438601+rafaucau@users.noreply.github.com>
2026-04-21 12:21:50 +0200

    docs: add comment explaining why CSS is not extracted

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -2,15 +2,15 @@ const { merge } = require('webpack-merge');
 const baseConfig = require('flarum-webpack-config')();
 
 module.exports = (env, argv) => {
-  const isProduction = argv.mode === 'production';
-
   return merge(
     baseConfig,
-    /** @type {import('webpack').Configuration}*/ {
+    /** @type {import('webpack').Configuration}*/
+    {
       module: {
         rules: [
           {
             test: /\.(sa|sc|c)ss$/,
+            // Flarum doesn't copy CSS from js/dist/forum, so we don't extract it and bundle it with the JavaScript.
             use: ['style-loader', 'css-loader'],
           },
         ],
